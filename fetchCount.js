78f2fce34ad88dfec43b67d3d118a1286f3e1b78
Nikita Tinaev <Nikkach1997@yandex.ru>
2022-12-05 01:56:11 +0300

    feat: добавь асинхронных задач

--- fetchCount.js
+++ fetchCount.js
@@ -1,22 +1,20 @@
 // напиши функцию, которая будет делать фетч гет запросы на урл,
 // и в случае ошибки (любой), пробовать ещё 5 раз, а потом выдавать ошибку Не получилось
 
-function fetchCustom(url) {
-  let count = 0;
+async function fetchCustom(url, counter = 0) {
+  let count = counter;
   let res = 0;
 
-  while (!Boolean(res) && count < 5) {
-    console.log(count);
-    console.log(res)
-    count+=1;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        res = data;
-        console.log(`получилось, count=${count}, res=${res}`);
-      })
-      .catch((err) => console.log(`ошибка, count=${count}`))
+  try {
+    count += 1;
+    let result = await fetch(url);
+    res = await result.json();
+  } catch {
+    if (count >= 5) console.log(`error poluchaetsa count=${count}`);
+    else fetchCustom(url, count);
   }
-}
 
-fetchCustom("https://randomuser.me/api/");
+  return res;
+}
+fetchCustom("https://randomuserrrr.me/api/");
+// fetchCustom("https://vk.com/feed");
