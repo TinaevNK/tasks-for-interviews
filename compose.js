b89d878c279a46cb6b1c10c543ed3d26a69431e4
Nikita Tinaev <Nikkach1997@yandex.ru>
2022-12-04 03:49:37 +0300

    feat: добавь самописный debounce

--- compose.js
+++ compose.js
@@ -1,21 +1,9 @@
-// const compose = (...fns) => (arg) => fns.reduceRight((composed, f) => f(composed), arg);
-
-const compose = (...fns) => (x) => fns.reduceRight((acc,fn)=>fn(acc), x);
+const compose = (...fns) => (arg) => fns.reduceRight((composed, f) => f(composed), arg);
 
 const one = (one) => console.log(one, "one");
 const two = (two) => {
   console.log(two, "two");
   return two + 'yeeees';
 };
-
-// const compose = function(args) {
-//   let funcs = Array.prototype.slice.call(args);
-
-//   return funcs.reduce(function(f,g) {
-//     return function() {
-//       return f(g.apply(this, args));
-//     };
-//   });
-// };
 
 compose(one, two)('первый пошёл');
